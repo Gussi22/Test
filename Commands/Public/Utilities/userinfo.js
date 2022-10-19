b6c6234bf464356bc96136fbe610b7f91c7fffb4
Gussi22 <samurai.adrian22@gmail.com>
2022-10-19 09:24:56 +0800

    userinfo implementation

---
+++ Commands/Public/Utilities/userinfo.js
@@ -0,0 +1,52 @@
+const { ContextMenuInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "userinfo",
+  type: "USER",
+  context: true,
+  /**
+   *
+   * @param {ContextMenuInteraction} interaction
+   */
+  async execute(interaction) {
+    const target = await interaction.guild.members.fetch(interaction.targetId);
+
+    interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setColor("RANDOM")
+          .setAuthor({
+            name: target.user.tag,
+            iconURL: target.user.avatarURL({ dynamic: true, size: 512 }),
+          })
+          .setThumbnail(target.user.avatarURL({ dynamic: true, size: 512 }))
+          .addFields(
+            {
+              name: "ID",
+              value: `${target.user.id}`,
+            },
+            {
+              name: "Roles",
+              value: `${
+                target.roles.cache
+                  .map((r) => r)
+                  .join(" ")
+                  .replace("@everyone", "") || "None"
+              }`,
+            },
+            {
+              name: "Member Since:",
+              value: `<t:${parseInt(target.joinedTimestamp / 1000)}:R>`,
+              inline: true,
+            },
+            {
+              name: "Discord User Since:",
+              value: `<t:${parseInt(target.user.createdTimestamp / 1000)}:R>`,
+              inline: true,
+            }
+          ),
+      ],
+      ephemeral: true,
+    });
+  },
+};
