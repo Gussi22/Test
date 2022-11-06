c682f1f23e2e33e1ab8ea60c3bde6744a688c0fd
Gussi22 <samurai.adrian22@gmail.com>
2022-11-06 10:31:56 +0800

    Cleaning of command interaction event

--- Events/Interaction/CommandInteraction.js
+++ Events/Interaction/CommandInteraction.js
@@ -9,50 +9,43 @@ module.exports = {
    * @param {Client} client
    */
   async execute(interaction, client) {
-    if (interaction.isCommand() || interaction.isContextMenu()) {
-      const command = client.commands.get(interaction.commandName);
-      if (!command)
-        return (
-          interaction.reply({
-            embeds: [
-              new MessageEmbed()
-                .setColor("RED")
-                .setDescription("🔴 An error has occurred"),
-            ],
-          }) && client.commands.delete(interaction.commandName)
-        );
+		if (!interaction.isCommand() || !interaction.isContextMenu()) return;
 
-      if (
-        command.permission &&
-        !interaction.member.permissions.has(command.permission)
-      ) {
-        return interaction.reply({
-          content: `You do not have the required permission for the command: \`${interaction.commandName}\``,
-          ephemeral: true,
-        });
-      }
+		const command = client.commands.get(interaction.commandName);
 
-      if (
-        command.ownerOnly &&
-        interaction.member.id !== interaction.guild.ownerId
-      ) {
-        return interaction.reply({
-          content: "You are not the owner of this server",
-          ephemeral: true,
-        });
-      }
+		if (!command) {
+			return interaction.reply({
+				embeds: [
+					new MessageEmbed()
+						.setColor("RED")
+						.setDescription("🔴 An error has occurred")
+				]
+			}) && client.commands.delete(interaction.commandName);
+		}
+		if (command.permission && !interaction.member.permissions.has(command.permission)) {
+			return interaction.reply({
+				content: `You do not have the required permission for the command \`${interaction.commandName}\``,
+				ephemeral: true
+			})
+		}
 
-      if (interaction.options.getSubcommand(false)) {
-        const subCommandFile = client.subCommands.get(
-          `${interaction.commandName}.${interaction.options.getSubcommand()}`
-        );
-        if (!subCommandFile)
-          return interaction.reply({
-            content: "This sub command is outdated!",
-            ephemeral: true,
-          });
-        subCommandFile.execute(interaction, client);
-      } else command.execute(interaction, client);
-    }
+		if (command.ownerOnly && interaction.member.id !== interaction.guild.ownerId) {
+			return interaction.reply({
+				content: "You are not the owner of this server",
+				ephemeral: true
+			})
+		}
+
+		if (interaction.options.getSubcommand(false)) {
+			const subCommandFile = client.subCommands.get(`${interaction.commandName}.${interaction.options.getSubcommand()}`);
+
+			if (!subCommandFile) {
+				return interaction.reply({
+					content: "This sub-command is outdated",
+					ephemeral: true
+				});
+			}
+			subCommandFile.execute(interaction, client);
+		} else command.execute(interaction, client);
   },
 };
